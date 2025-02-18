3beda6142e63a2638ae8d675161cf56bb1279dbc
Mark Manipula <markmikomanipula@gmail.com>
2025-02-17 20:27:26 -0800

    Setup create event api

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use('/events', eventRoutes);
 connectDB();
 
 // Start server
-const port = 5000;
+const port = process.env.PORT || 5001;  // Change to a different port, like 5001
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
